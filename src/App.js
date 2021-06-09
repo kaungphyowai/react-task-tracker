efcb07f6d2fb1622566312f83268bba909b83b06
Kaung-Phyo-Wai <kaungphyowaidecember@gmail.com>
2021-06-10 02:53:58 +0630

    delete tasks and props drilling

--- src/App.js
+++ src/App.js
@@ -17,10 +17,16 @@ function App() {
       "reminder": true
     }
   ]);
+
+  //delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id))
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : "There is no task"}
     </div>
   );
 }
