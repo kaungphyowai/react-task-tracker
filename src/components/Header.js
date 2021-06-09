c53bdb73f18eb52f5b3d1189d84193202cc41132
Kaung Phyo Wai <kaungphyowaidecember@gmail.com>
2021-06-09 12:26:47 +0630

    Finished learning how props works

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
-
-const Header = () => {
+import PropType from 'prop-types'
+const Header = ({ title }) => {
     return (
         <header>
-            <h1>Task Tracker</h1>
+            <h1>{title}</h1>
         </header>
     )
 }
+Header.defaultProps = {
+    "title": "Task Trackder"
+}
 
+Header.propTypes = {
+    "title": PropType.string.isRequired,
+}
 export default Header
