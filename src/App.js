f9a231879b90f02c22316b3c1a9674af30db3623
Kaung-Phyo-Wai <kaungphyowaidecember@gmail.com>
2021-06-10 17:24:53 +0630

    add task submit

--- src/App.js
+++ src/App.js
@@ -18,6 +18,12 @@ function App() {
       "reminder": true
     }
   ]);
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
 
   //delete task
   const deleteTask = (id) => {
@@ -30,7 +36,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : "There is no task"}
     </div>
   );
