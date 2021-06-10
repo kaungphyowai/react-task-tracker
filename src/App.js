9d497bb17a89e57a802815f1dc6ec8d28e02dd81
Kaung-Phyo-Wai <kaungphyowaidecember@gmail.com>
2021-06-10 18:45:07 +0630

    toggle reminder on server

--- src/App.js
+++ src/App.js
@@ -24,14 +24,21 @@ function App() {
 
   useEffect(() => {
     const getTasks = async () => {
-      const tasksFromServer = await fetchTask()
+      const tasksFromServer = await fetchTasks()
       setTasks(tasksFromServer)
     }
     getTasks()
   }, [])
-  //fetch task
-  const fetchTask = async () => {
+  //fetch tasks
+  const fetchTasks = async () => {
     const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json();
+    return data
+  }
+
+  //fetch task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
     const data = await res.json();
     return data
   }
@@ -41,8 +48,20 @@ function App() {
     setTasks(tasks.filter(task => task.id !== id))
   }
   //Toggle Reminder
-  const toggleReminder = (id) => {
-    setTasks(tasks.map(task => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+    const res = await fetch(`http://localhost:5000/tasks/${id}`,
+      {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(updTask)
+      }
+    )
+    const data = await res.json()
+    setTasks(tasks.map(task => task.id === id ? { ...task, reminder: data.reminder } : task))
   }
   return (
     <div className="container">
