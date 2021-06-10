d45c622249f6c4b15b9754462723214f3da5e615
Kaung-Phyo-Wai <kaungphyowaidecember@gmail.com>
2021-06-10 18:28:39 +0630

    delete task from server

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
   }
   //delete task
   const deleteTask = async (id) => {
-
+    await fetch(`http://localhost:5000/tasks/${id}`, { method: 'DELETE' },)
     setTasks(tasks.filter(task => task.id !== id))
   }
   //Toggle Reminder
